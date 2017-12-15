06d9b66b3a3d785158fc108c430d45cd08c06651
zubarevnka <zubarevnka@mail.ru>
2017-12-16 00:03:26 +0500

    +Laba

---
+++ laba2/FormTask/Controllers/ContactController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using FormTask.Models;
+using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MimeKit;
+
+namespace FormTask.Controllers
+{
+    public class ContactController : Controller
+    {
+        private static bool _success = false;
+        private static IConfigurationRoot Configuration { get; set; }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Send(MessageModel email)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Index");
+            }
+            try
+            {
+                ViewBag.ErrorMessage = null;
+                var builder = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json");
+                Configuration = builder.Build();
+
+                var adminAdress = Configuration["SMTP:Admin"];
+                var adress = Configuration["SMTP:Email"];
+                var host = Configuration["SMTP:Host"];
+                var port = Convert.ToInt32(Configuration["SMTP:Port"]);
+                var password = Configuration["SMTP:password"];
+
+                var message = new MimeMessage();
+                message.From.Add(new MailboxAddress(email.Address, adress));
+                message.To.Add(new MailboxAddress(adminAdress));
+                message.Subject = email.Topic;
+                message.Body = new TextPart("plain")
+                {
+                    Text = email.Text
+                };
+
+                using (var smtpClient = new SmtpClient())
+                {
+                    smtpClient.Connect(host, port, true);
+                    smtpClient.Authenticate(adress, password);
+                    smtpClient.Send(message);
+                    smtpClient.Disconnect(true);
+                    _success = true;
+                }
+            }
+            catch (Exception e)
+            {
+                _success = false;
+                ViewBag.ErrorMessage = e.Message;
+            }
+            ViewBag.Success = _success;
+            return View("Index");
+        }
+    }
+}
---
+++ laba2/FormTask/Models/MessageModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FormTask.Models
+{
+    public class MessageModel
+    {
+        [Required(ErrorMessage = "Необходимо ввести адрес")]
+        [EmailAddress(ErrorMessage = "Неверный формат email")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Введите тему")]
+        [StringLength(50, ErrorMessage = "Тема не должна превышать 50 символов")]
+        public string Topic { get; set; }
+
+        [Required(ErrorMessage = "Введите сообщение")]
+        [StringLength(2000, ErrorMessage = "Сообщение не должно превышать 2000 символов.")]
+        public string Text { get; set; }
+    }
+}
